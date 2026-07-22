7ff93c63045671df759d4e88ae34c89ac2ce4f52
devidsmith9527-crypto <soeurnglim@gmail.com>
2026-07-22 18:30:09 +0700

    Update BookController.cs

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -34,6 +34,50 @@ namespace APIApplication.Controllers
             }
             return Ok(book);
         }
+
+        [HttpPost]
+        public IActionResult CreateBook([FromBody] Book book)
+        {
+            if (book == null)
+            {
+                return BadRequest();
+            }
+            var books = _books();
+            book.BookId = books.Max(b => b.BookId) + 1;
+            books.Add(book);
+            return CreatedAtAction(nameof(GetBook), new { id = book.BookId }, book);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book book)
+        {
+            if (book == null)
+            {
+                return BadRequest();
+            }
+            var books = _books();
+            var existingBook = books.FirstOrDefault(b => b.BookId == id);
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+            books.Remove(existingBook);
+            book.BookId = id;
+            books.Add(book);
+            return Ok(book);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteBook(int id)
+    {
+        var books = _books();
+        var book = books.FirstOrDefault(b => b.BookId == id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+        books.Remove(book);
+        return Ok(book);
     }
 
 }
